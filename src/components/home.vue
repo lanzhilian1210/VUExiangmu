5db13cd2d43ccc693b3c556cf9cbb0ef1de1f401
namedadalian <1354892101@qq.com>
2018-08-27 21:08:56 +0800

    项目1

---
+++ src/components/home.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="pageWapper">
+    <div class="header-wrapper">
+        <div class="logo"></div>
+        <div class="logoText">文档转换真简单</div>
+    </div>
+</div>
+</template>
+<style>
+.pageWapper{
+    display: table;
+    height: 100%;
+    width: 100%;
+}
+.logo{
+    width: 160px;
+    height: 40px;
+    background:dodgerblue;
+    display: inline-block;
+}
+.logoText{
+    height: 16px;
+    font:16px/16px "微软雅黑";
+    color: #333;
+    position: absolute;
+    left: 250px;
+    top: 70px;
+}
+</style>
